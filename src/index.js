4d393754c903856482d88c20985a25a2e821ee66
captern <captern@capterndeMacBook-Pro.local>
2018-09-25 18:38:50 +0800

    change

--- src/index.js
+++ src/index.js
@@ -5,14 +5,19 @@ import './index.css';
 import './components/globalcss/init.scss'
 import App from './route/root';
 import registerServiceWorker from './registerServiceWorker';
-
+import * as imgUrls from './components/imgurls'
 // M站快速点击
 import FastClick from 'fastclick'
 
 FastClick.attach(document.body);
 
-
-// // loading动画dom
+// 预加载图片
+let myloadingimgs = []
+for(let i in imgUrls){
+  myloadingimgs.push(imgUrls[i])
+}
+console.log(myloadingimgs)
+// loading动画dom
 var loadingDiv = document.getElementById('pageloading')
 // 判断浏览器兼容前缀
 var useTransform = (loadingDiv.style['msTransform'] && 'msTransform')
@@ -20,8 +25,30 @@ var useTransform = (loadingDiv.style['msTransform'] && 'msTransform')
   || (loadingDiv.style['MozTransform'] && 'MozTransform')
   || (loadingDiv.style['OTransform'] && 'OTransform')
   || 'transform';
-// loadingDiv.style[useTransform] = styles;
+var imgloadindex = 0        // 已经加载图片数量
+var imgNum = 0              // 图片总数
+myloadingimgs.map(urls => {
+  for(let i in urls){
+    imgNum ++
+    let img = new Image()
+    img.onload = () => {
+      // 当图片加载成功时，计数加一
+      imgloadindex++
+      reactloading()
+    }
+    img.src = urls[i]
+  }
+})
 
+function reactloading(){
+  const styles = 'scaleX(' + (imgloadindex / (imgNum - 3)).toFixed(2) + ')';
+  loadingDiv.style[useTransform] = styles;
+  if(imgloadindex >= imgNum-1){
+    ReactDOM.render(<App />, document.getElementById('root'));
+    console.log('1212')
+    loadingDiv = myloadingimgs = useTransform = imgloadindex = imgNum = null
+  }
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+}
+// ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
