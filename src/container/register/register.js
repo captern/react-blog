577bcf12dd427cabc2ea01bd619e3129778542b8
captern <captern@capterndeMacBook-Pro.local>
2018-09-29 19:23:51 +0800

    change

--- src/container/register/register.js
+++ src/container/register/register.js
@@ -6,8 +6,21 @@ class Register extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      user: '',
+      pwd: '',
+      repeatPwd: '',
       type: 'genius'
     }
+    this.handleRegister = this.handleRegister.bind(this)
+  }
+
+  handleChange(key, val) {
+    this.setState({
+      [key]: val
+    })
+  }
+  handleRegister(){
+    console.log(this.state)
   }
 
   render() {
@@ -17,13 +30,19 @@ class Register extends React.Component {
         <Logo/>
         <h2>注册页面</h2>
         <List>
-          <InputItem>用户名</InputItem>
-          <InputItem>密码</InputItem>
-          <InputItem>确认密码</InputItem>
-          <RadioItem checked={this.state.type === 'genius'}>牛人</RadioItem>
-          <RadioItem checked={this.state.type === 'boss'}>Boss</RadioItem>
+          <InputItem onChange={v => this.handleChange('user', v)}>用户名</InputItem>
+          <InputItem onChange={v => this.handleChange('pwd', v)}>密码</InputItem>
+          <InputItem onChange={v => this.handleChange('repeatPwd', v)}>确认密码</InputItem>
+          <RadioItem
+            checked={this.state.type === 'genius'}
+            onChange={()=>this.handleChange('type','genius')}
+          >牛人</RadioItem>
+          <RadioItem
+            checked={this.state.type === 'boss'}
+            onChange={()=>this.handleChange('type','boss')}
+          >Boss</RadioItem>
           <WhiteSpace/>
-          <Button type='primary'>注册</Button>
+          <Button type='primary' onClick={this.handleRegister}>注册</Button>
         </List>
       </div>
     )
