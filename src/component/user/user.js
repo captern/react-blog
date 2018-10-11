aa910b6cf1b049a47e20ff78712b83c97f8cdee3
captern <captern@capterndeMacBook-Pro.local>
2018-10-11 11:42:41 +0800

    个人信息完全展示

--- src/component/user/user.js
+++ src/component/user/user.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {Result} from 'antd-mobile'
+import {Result, List, WhiteSpace} from 'antd-mobile'
 
 @connect(
   state => state.user
@@ -8,7 +8,8 @@ import {Result} from 'antd-mobile'
 class User extends React.Component {
   render() {
     const props = this.props;
-    console.log(props)
+    const Item = List.Item;
+    const Brief = Item.Brief;
     return props.user ? (
       <div>
         <Result
@@ -16,7 +17,18 @@ class User extends React.Component {
           title={props.user}
           message={props.type === 'boss' ? props.company : null}
         />
-        <p>用户中心</p>
+        <List renderHeader={() => '简介'}>
+          <Item multipleLine>
+            {props.title}
+            {props.desc.split('/n').map(v => <Brief key={v}>{v}</Brief>)}
+            {props.money?<Brief>薪资:{props.money}</Brief>:null}
+            {/*<Brief>{this.props.title}</Brief>*/}
+          </Item>
+        </List>
+        <WhiteSpace/>
+        <List>
+          <Item>退出登录</Item>
+        </List>
       </div>
     ) : null
   }
